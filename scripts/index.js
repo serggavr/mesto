248006c6a29518bb5025c3274cbf64e024b51e5d
GSergey <g.sergey@bk.ru>
2022-04-06 23:43:11 +0300

    feature/add createCard function & card template

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,70 @@ const popupCloseBtn = document.querySelector('.popup__close-button');
 const popupForm = document.querySelector('.popup__form')
 const popupTitle = document.querySelector('.popup__text-input_change_profile-title');
 const popupSubtitle = document.querySelector('.popup__text-input_change_profile-subtitle');
+const elementList = document.querySelector('.elements__list')
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+  
+function createCard(name, link) {
+    const elementTemplate = document.querySelector('#element').content;
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+
+    element.querySelector('.element__photo').src = link;
+    element.querySelector('.element__photo').alt = name;
+    element.querySelector('.element__title').textContent = name;
+
+    // Для удаления, назначить на кнопку удаления, пока повесил на element if title
+    element.querySelector('.element__title').addEventListener('click', (e) => {
+      e.target.closest('.element').remove()
+    })
+    //
+
+    // Для лайков
+    element.querySelector('.element__like').addEventListener('click', (e) => {
+      e.target.classList.toggle('element__like_active')
+    })
+    //
+
+    // Для popup
+    element.querySelector('.element__photo').addEventListener('click', (e) => {
+
+    })
+    //
+
+    return element
+};
+
+function addElementToList(arr) {
+  arr.forEach((elem) => {
+    elementList.appendChild(createCard(elem.name, elem.link))
+  })
+}
 
 function togglePopup() {
   popup.classList.toggle('popup_opened')
@@ -23,6 +86,10 @@ function changeProfileContent(event) {
   profileTitle.textContent = popupTitle.value
   profileSubtitle.textContent = popupSubtitle.value
   togglePopup()
+}
+
+window.onload = function() {
+  addElementToList(initialCards)
 }
 
 
